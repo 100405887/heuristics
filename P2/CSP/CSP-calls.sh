eff37e4ef5ea0a150134d0e29c3d0ed459465520
100405887 <100405887@alumnos.uc3m.es>
2021-12-18 12:24:45 +0100

    to-do y mas pruebas

--- P2/CSP/CSP-calls.sh
+++ P2/CSP/CSP-calls.sh
@@ -2,5 +2,4 @@ python3 CSPStowage.py data mapE containerE
 python3 CSPStowage.py data map2 containerE
 python3 CSPStowage.py data mapE container2
 python3 CSPStowage.py data map2 container2
-
-
+python3 CSPStowage.py data mapS containerS
